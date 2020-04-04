6d048a2cf373e1713ca57d17bd4522cc0af93bc2
Misha Muzyka <muzyka.m.00gmail.com>
2020-04-04 16:55:01 +0300

    asp.net core added

---
+++ RestavrationService/RestavrationService/Models/Repositories/IGenericRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RestavrationService.Models.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void Create(T corpus);
+        void Clear();
+    }
+}
